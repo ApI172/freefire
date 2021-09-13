28dce1a2acb623b282a10026ead6c2fdf90ad40c
ApI172 <rahul.s.s@apisero.com>
2021-09-13 12:44:03 +0530

    my commit

---
+++ app.java
@@ -0,0 +1,14 @@
+package com.p25;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	
+        System.out.println( "Rahul" );
+    }
